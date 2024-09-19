76a7c4fb9aca81187b84dd1b362a5a734d46f58a
billy <billy@twetch.com>
2024-09-18 19:24:49 -0500

    save

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,4 @@
 'use client';
-
-import Link from "next/link";
-import Image from "next/image";
-import { Github, Twitter } from 'lucide-react';
-
 export default function Header() {
   return (
     <header>
