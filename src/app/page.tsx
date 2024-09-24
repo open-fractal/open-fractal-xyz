c72b5b30e93a84dce82e76f96c0566701c53779c
coinyeezy <joshpetty@proton.me>
2024-09-24 17:47:39 -0400

    idk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,6 @@ import ApiSection from "@/components/ApiSection"
 import FrttrSection from "@/components/FrttrSection"
 
 export default function Home() {
-  const frttrItems = [
-    { title: "Feature 1", description: "Description of feature 1" },
-    { title: "Feature 2", description: "Description of feature 2" },
-    { title: "Feature 3", description: "Description of feature 3" },
-  ];
-
   return (
     <>
       <Header />
