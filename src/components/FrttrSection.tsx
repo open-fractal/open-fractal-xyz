7aca8426ad7a7c76cdb53caf2a56e1613f0f9f5e
coinyeezy <joshpetty@proton.me>
2024-09-24 17:40:51 -0400

    idk

--- src/components/FrttrSection.tsx
+++ src/components/FrttrSection.tsx
@@ -7,7 +7,7 @@ interface FrttrSectionProps {
   items: { title: string; description: string }[]
 }
 
-const FrttrSection = ({ title, _items }: FrttrSectionProps) => {
+const FrttrSection = ({ title, items }: FrttrSectionProps) => {
   return (
     <section className="relative w-full py-12 md:py-24 lg:py-32 xl:py-48 overflow-hidden bg-white flex items-center justify-center">
       <div 
